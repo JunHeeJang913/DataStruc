75c00dfd0753a73a7679cb180b1ab72469010f8b
Jang JunHee <lyradortiz913@naver.com>
2020-11-02 23:49:58 +0900

    hw7_advancing

--- hw7/src/bt.h
+++ hw7/src/bt.h
@@ -9,9 +9,11 @@ struct Node
 {
     Node(T d, Node<T> *left = 0, Node<T> *right = 0)
         : data(d), leftChild(left), rightChild(right) {}
+    bool leftThreaded=true;
     Node<T> *leftChild;
     T data;
     Node<T> *rightChild;
+    bool rightThreaded=true;
 };
 template <class T>
 class Tree
@@ -37,25 +39,56 @@ void Tree<T>::Visit(Node<T> *ptr) { cout << ptr->data << " "; }
 template <class T>
 void Tree<T>::Insert(Node<T> *&ptr, T &value)
 { //Insert 의 helper 함수
-    if (ptr == 0)
-        ptr = new Node<T>(value);
-    else if (value < ptr->data)
+    static Node<T> * temp=0;
+    static bool check = 0;
+    if(ptr==0){//조건 고치기
+        if(ptr==0&&temp==0){
+            ptr = new Node<T>(value);
+            temp=ptr;
+        }
+        else if ((check==0&&temp->leftThreaded==true)||(check==1&&temp->rightThreaded==true)){   
+            ptr = new Node<T>(value);
+            if(temp!=0){
+                if(check==0){
+                    if(temp->leftChild!=ptr)
+                        ptr->leftChild=temp->leftChild;
+                    ptr->rightChild=temp;
+                    temp->leftThreaded=false;
+                }
+                else if(check==1){
+                    if(temp->rightChild!=ptr)
+                        ptr->rightChild=temp->rightChild;
+                    ptr->leftChild=temp;                
+                    temp->rightThreaded=false;
+                }
+            }
+        }
+    }
+    else if (value < ptr->data){
+        check = 0;
+        temp=ptr;
         Insert(ptr->leftChild, value);
-    else if (value > ptr->data)
+    }
+    else if (value > ptr->data){
+        check = 1;
+        temp=ptr;
         Insert(ptr->rightChild, value);
+    }
     else
-        cout << endl
-             << "Duplicate value " << value << " ignored\n";
+        cout<< "Duplicate value " << value << " ignored\n"<< endl;
+
 }
 
 template <class T>
 void Tree<T>::Preorder(Node<T> *ptr){
     if(ptr){
         Visit(ptr);
-        Preorder(ptr->leftChild);
-        Preorder(ptr->rightChild);
+        if(ptr->leftThreaded==false)
+            Preorder(ptr->leftChild);
+        if(ptr->rightThreaded==false)
+            Preorder(ptr->rightChild);
     }
-}
+}//재귀적인 방법
 
 template <class T>
 void Tree<T>::Inorder(Node<T> *ptr){
@@ -71,7 +104,7 @@ void Tree<T>::Inorder(Node<T> *ptr){
         Visit(currentNode);
         currentNode = currentNode->rightChild;
     }
-}
+}//비재귀적방법
 
 template <class T>
 void Tree<T>::Postorder(Node<T> *ptr){
