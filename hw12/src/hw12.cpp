f003342c8f793dcb82ca95b8bafc756386d470fd
Jang JunHee <lyradortiz913@naver.com>
2020-12-01 23:32:05 +0900

    recursiveMerge, naturalMerge left

--- hw12/src/hw12.cpp
+++ hw12/src/hw12.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
+#include <stdlib.h>
+#include <sys/time.h>
 #include "sort.h"
 using namespace std;
+struct timeval start_t, end_t;
 int main(int argc, char *argv[])
 {
     int T = atoi(argv[1]); // num of test case
     cout << "T=" << T << endl;
     int N;             // 각 테스트케이스 별 레코드의 길이
     int i;             // iterator
-    clock_t result[6]; // result 배열에 각 알고리즘 별로 실행시간을 저장하게 됩니다.
+    double result[6]; // result 배열에 각 알고리즘 별로 실행시간을 저장하게 됩니다.
+    double diff_t;
     // result[0]: insertion sort
     // result[1]: quick sort
     // result[2]: iterative merge sort
     // result[3]: reculsive merge sort
     // result[4]: natural merege sort
     // result[5]: heap sort
-    clock_t start;
-    clock_t end;
+    
     if (argc < 3)
     {
         cerr << "wrong argument count" << endl;
@@ -26,15 +29,77 @@ int main(int argc, char *argv[])
     cout << "--INS--|-QUICK-|-MERGE-|-RECMG-|-NATMG-|-HEAP--| " << endl;
     for (i = 2; i < T + 2; i++)
     {
+        fstream fin(argv[i]);
+        fin>>N;
+        int * ins= new int [N+1];
+        int * quick=new int [N+1];
+        int * merge=new int [N+1];
+        int * recmg=new int [N+1];
+        int * natmg=new int [N+1];
+        int * heap=new int [N+1];
+        for(int j=1;j<=N;++j){
+            fin>>ins[j];
+            quick[j]=ins[j];
+            merge[j]=ins[j];
+            recmg[j]=ins[j];
+            natmg[j]=ins[j];
+            heap[j]=ins[j];
+        }
+        fin.close();
+
         // i번째 인자의 파일을 읽습니다.
         // 각 정렬 알고리즘에 필요한 자료구조를 생성하고 데이터를 담습니다.
         // 여기부터 정렬 시간 측정을 시작합니다.
-        /* example
-        start = clock();
-        삽입정렬 수행
-        end = clock();
-        result[0] = end-start; */
+        
+        gettimeofday(&start_t, NULL); 
+        //삽입정렬 수행 ...
+        InsertionSort(ins,N);
+        gettimeofday(&end_t, NULL); 
+        printArray(ins, N);
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[0] = diff_t;
+
+        gettimeofday(&start_t, NULL); 
+        //퀵정렬 수행 ...
+        QuickSort(quick, 1, N);
+        gettimeofday(&end_t, NULL); 
+        printArray(quick, N);
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[1] = diff_t;
+
+        gettimeofday(&start_t, NULL); 
+        //합병정렬 수행 ...
+        MergeSort(merge, N);
+        gettimeofday(&end_t, NULL); 
+        printArray(merge, N);
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[2] = diff_t;
+
+        gettimeofday(&start_t, NULL); 
+        //순환합병정렬 수행 ...
+        int * link = new int [N+1];
+        rMergeSort(recmg, link,1,N);
+        gettimeofday(&end_t, NULL); 
+        printArray(recmg, N);
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[3] = diff_t;
+
+        gettimeofday(&start_t, NULL); 
+        //자연합병정렬 수행 ...
+        gettimeofday(&end_t, NULL); 
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[4] = diff_t;
+
+        gettimeofday(&start_t, NULL); 
+        //힙정렬 수행 ...
+        HeapSort(heap, N);
+        gettimeofday(&end_t, NULL); 
+        printArray(heap,N);
+        diff_t = (double)(end_t.tv_sec-start_t.tv_sec)+((double)(end_t.tv_usec-start_t.tv_usec)/1000000); 
+        result[5] = diff_t;
+        
         // 결과를 출력합니다. (이 부분은 수정하지 않습니다)
+        
         cout.precision(5);
         cout << fixed;
         for (int j = 0; j < 6; j++)
@@ -42,5 +107,13 @@ int main(int argc, char *argv[])
             cout << (double)result[j] / CLOCKS_PER_SEC << "|";
         }
         cout << "N=" << N << endl;
+
+        //할당해제
+        delete [] ins;
+        delete [] quick;
+        delete [] merge;
+        delete [] recmg;
+        delete [] natmg;
+        delete [] heap;
     }
 }
--- hw12/src/sort.h
+++ hw12/src/sort.h
@@ -1,7 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
 #include <iostream>
+#include <algorithm>
+using namespace std;
 //삽입정렬, n개의 원소가 있다고치면, 1부터 n까지 인덱스 사용
 template<class T>
 void Insert(const T& e, T*a, int i){
+    //새로 들어온 값을 기존 배열 요소와 각각 비교해가며,
+    //적당한 위치에 삽입
     a[0]=e;
     while(e<a[i]){
         a[i+1]=a[i];
@@ -11,8 +17,138 @@ void Insert(const T& e, T*a, int i){
 }
 template<class T>
 void InsertionSort(T*a, const int n){
+    //Insert함수를 반복적으로 불러내서
+    //배열을 정렬함
     for(int j=2; j<=n;j++){
         T temp=a[j];
         Insert(temp,a,j-1);
     }
 }
+//퀵정렬
+template <class T>
+void QuickSort(T *a, const int left, const int right){
+    //정렬할 레코드 중 피벗(중추) 레코드 선택
+    //정렬할 레코드들을 다시 정돈
+    //퀵 정렬을 recursive하게 사용
+    if(left<right){
+        int i=left,j=right+1,pivot=a[left];
+        do{
+            do i++; while(a[i]<pivot);
+            do j--; while(a[j]>pivot);
+            if(i<j) swap(a[i],a[j]);
+        }while(i<j);//피벗 선택(i==j, i>j)과, 정렬할 래코드 재정돈.
+        swap(a[left],a[j]);//피벗이 중간점에
+
+        QuickSort(a,left,j-1);  //피벗기준 좌측 배열 정렬
+        QuickSort(a,j+1, right);    //피벗기준 우측 배열 정렬
+    }
+}
+//합병정렬-반복합병정렬(Iterative Merge Sort)
+template<class T>
+void Merge(T*initList, T*mergedList, const int l, const int m, const int n){
+    //두개의 정렬된 리스트(initList[l:m],initList[m+1:n])를 하나의 정렬된 리스트(mergedList)로 합병
+    int i1, iResult, i2;
+    for(i1=l, iResult=l, i2=m+1;i1<=m&&i2<=n;iResult++){
+        //1->정렬리스트1, 2->정렬리스트2
+        if(initList[i1]<=initList[i2]){
+            mergedList[iResult]=initList[i1];
+            i1++;
+        }
+        else{
+            mergedList[iResult]=initList[i2];
+            i2++;
+        }
+    }
+    //위 과정에서 각 리스트에 남은 원소들이 있다면, 복사해준다.
+    copy(initList+i1,initList+m+1, mergedList+iResult);
+    copy(initList+i2,initList+n+1, mergedList+iResult);
+}
+template <class T>
+void MergePass(T *initList,T*resultList, const int n , const int s){
+    //합병정렬은 여러개의 합병단계(pass)로 구분된다.
+    //initList가 s개 씩 두개로 쪼개진 배열이다. resultList는 2*s 단위로 정렬됨.
+    int i;
+    for(i=1; i<=n-2*s+1;i+=2*s)
+        Merge(initList, resultList, i,i+s-1, i+2*s-1);
+    if((i+s-1)<n)
+        Merge(initList,resultList,i,i+s-1,n);
+    else
+        copy(initList+i,initList+n+1,resultList+i);
+}
+template<class T>
+void MergeSort(T*a, const int n){
+    T * tempList = new T[n+1];
+    for(int l=1; l<n;l*=2){
+        MergePass(a, tempList, n, l);
+        l *=2;
+        MergePass(tempList,a,n,l);
+    }
+    delete [] tempList;
+}
+
+//합병정렬-순환
+template<class T>
+int ListMerge(T*a, int * link, const int start1, const int start2){
+    int iResult = 0;
+    for(int i1=start1,i2=start2;i1&&i2;){
+        if(a[i1]<=a[i2]){
+            link[iResult]=i1;
+            iResult=i1; i1=link[i1];
+        }
+        else{
+            link[iResult]=i2;
+            iResult=i2; i2=link[i2];
+        }
+        if(i1==0) link[iResult]=i2;
+        else link[iResult]=i1;
+        return link[0];
+    }
+}
+
+template <class T>
+int rMergeSort(T*a, int *link, const int left, const int right){
+    if(left>=right) return left;
+    int mid=(left+right)/2;
+    return ListMerge(a,link,rMergeSort(a,link,left,mid),rMergeSort(a,link,mid+1,right));
+}
+
+
+
+//합병정렬-자연
+
+
+
+//힙정렬
+template <class T>
+void Adjust(T*a, const int root, const int n){
+    T e=a[root];
+    int j;
+    for(j=2*root; j<=n; j*=2){
+        if(j<n&&a[j]<a[j+1]) j++;
+        if(e>=a[j]) break;
+        a[j/2]=a[j];
+    }
+    a[j/2]=e;
+}
+
+template <class T>
+void HeapSort(T*a, const int n){
+    for(int i=n/2;i>=1;i--)
+        Adjust(a,i,n);
+    for(int i=n-1;i>=1;i--){
+        swap(a[1],a[i+1]);
+        Adjust(a,1,i);
+    }
+}
+//
+template <class T>
+void printArray(T*a, const int n){
+    for(int i=1;i<=n;++i){
+        cout<<a[i]<<" ";
+        if(i%10==0)
+            cout<<endl;
+    }
+    cout<<endl;
+}
+
+#endif
